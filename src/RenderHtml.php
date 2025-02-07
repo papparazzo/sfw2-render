765e78cf10257e4faf6647181cac6b5d9333734f
papparazzo <pappi-@gmx.de>
2025-02-07 05:49:17 +0100

    wip...

---
+++ src/RenderHtml.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SFW2\Render;
+
+use Handlebars\Handlebars;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+final class RenderHtml implements RenderInterface
+{
+    public function __construct(
+        private readonly Handlebars $handlebars,
+        private readonly string $skeleton,
+        private readonly bool $appendAttributes = false
+    ) {
+    }
+
+    public function render(Request $request, Response $response, array $data = [], ?string $template = null): Response
+    {
+        $data = $this->appendAttributes ? array_merge($request->getAttributes(), $data) : $data;
+        if($template !== null) {
+            $payload = $this->handlebars->render($template, $data);
+            $data['content'] = $payload;
+        }
+        $payload = $this->handlebars->render($this->skeleton, $data);
+        $response->getBody()->write($payload);
+
+        return $response;
+    }
+}
