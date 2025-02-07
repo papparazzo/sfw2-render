08131bf27dd6c74b2ee614d76153174196d5df23
papparazzo <pappi-@gmx.de>
2025-02-07 05:45:08 +0100

    wip...

---
+++ src/RenderOnConditions.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SFW2\Render;
+
+use OutOfRangeException;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use SFW2\Render\Conditions\ConditionInterface;
+
+class RenderOnConditions implements RenderInterface
+{
+    /**
+     * @var list<array{on: ConditionInterface, render: RenderInterface}>
+     */
+    protected array $engines = [];
+
+    public function addEngine(ConditionInterface $on, RenderInterface $engine): self
+    {
+        $this->engines[] = ['on' => $on, 'render' => $engine];
+        return $this;
+    }
+
+    public function render(Request $request, Response $response, array $data = [], ?string $template = null): Response
+    {
+        foreach ($this->engines as $entry) {
+            if ($entry['on']($request, $response)) {
+                return $entry['render']->render($request, $response, $data, $template);
+            }
+        }
+        throw new OutOfRangeException("no render engines found");
+    }
+}
---
+++ src/RenderXml.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SFW2\Render;
+
+use Handlebars\Handlebars;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+final class RenderXml implements RenderInterface
+{
+    public function __construct(
+        private readonly Handlebars $handlebars,
+        private readonly bool $appendAttributes = false
+    ) {
+    }
+
+    public function render(Request $request, Response $response, array $data = [], ?string $template = null): Response
+    {
+        $data = $this->appendAttributes ? array_merge($request->getAttributes(), $data) : $data;
+        $payload =
+            '<?xml version="1.0" encoding="utf-8"?>' . PHP_EOL .
+            '<div>' .
+            $this->handlebars->render($template, $data) .
+            '</div>';
+
+        $response->getBody()->write($payload);
+        return $response->withHeader('Content-Type', 'text/xml');
+    }
+}
