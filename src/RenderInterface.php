ee8c06dbae130e14bfb5e29de8129280b442a33f
papparazzo <pappi-@gmx.de>
2025-02-09 07:23:39 +0100

    phpstan

--- src/RenderInterface.php
+++ src/RenderInterface.php
@@ -7,5 +7,12 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 interface RenderInterface
 {
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array<string, mixed> $data
+     * @param string|null $template
+     * @return Response
+     */
     public function render(Request $request, Response $response, array $data = [], ?string $template = null): Response;
 }
